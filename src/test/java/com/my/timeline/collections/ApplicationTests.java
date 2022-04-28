4dbfbd2d0ed182eff41020d816fb80b80598a5b6
jsing <joosing711@gmail.com>
2022-04-28 20:57:08 +0900

    Init the project

---
+++ src/test/java/com/my/timeline/collections/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.timeline.collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
